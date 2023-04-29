a2e578f201804ab38359ca8f42b62dec62fadabf
VladaMalysheva <ik02.vlada.m@gmail.com>
2023-04-29 21:04:41 +0300

    started doing unit tests

---
+++ src/test/java/com/epam/esm/service/TagServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.epam.esm.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagServiceImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
